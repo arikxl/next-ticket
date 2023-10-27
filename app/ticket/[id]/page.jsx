8435b2d2ac54c05ab38898b060cd19487f53712f
Arik Alexandrov <arikxl@gmail.com>
2023-10-27 23:23:03 +0300

    deploy1

--- app/ticket/[id]/page.jsx
+++ app/ticket/[id]/page.jsx
@@ -1,4 +1,4 @@
-import Form from '@/app/(components)/form';
+import Form from '../../(components)/Form';
 
 const getTicketById = async (id) => {
   const res = await fetch(`http://localhost:3000/api/tickets/${id}`, {
